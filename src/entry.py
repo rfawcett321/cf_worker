f39064ffec71c716ff9b989a449d8af3e601f665
rich <rfawcett87@gmail.com>
2025-12-11 18:36:27 +0000

    Fixed country selection order

--- src/entry.py
+++ src/entry.py
@@ -11,21 +11,18 @@ class Default(WorkerEntrypoint):
 
         request_headers = dict(request.headers)
         request_country = "the moon"
+        request_user= "bob"
+
+        if request_headers.get("cf-access-authenticated-user-email") != None :
+            request_user = request_headers["cf-access-authenticated-user-email"]
+
+        if request_headers.get("cf-ipcountry") != None :
+            request_country = request_headers["cf-ipcountry"]
 
         if path in ["/", "/secure"]:
 
             request_time = datetime.datetime.now()
-            request_user= "bob"
-
-
-            if request_headers.get("cf-access-authenticated-user-email") != None :
-                request_user = request_headers["cf-access-authenticated-user-email"]
-
-            if request_headers.get("cf-ipcountry") != None :
-                request_country = request_headers["cf-ipcountry"]
-            
             response_payload = request_user + " authenticated at " + str(request_time) + " from " + request_country
-
 
             return Response(response_payload)
         
@@ -36,14 +33,14 @@ class Default(WorkerEntrypoint):
             country_path = "gb"
 
             if len(request_country) == 2:
-                country_path = request_country + ".svg"
+                country_path = request_country.lower() + ".svg"
             else:
-                return Response("Object not found", status=404)
+                return Response("Object not right length - " + request_country, status=404)
 
             r2_pic = await r2_bucket.get(country_path)
 
             if r2_pic is None:
-                return Response("Object not found", status=404)
+                return Response("Object not found - " + country_path, status=404)
             
             content = await r2_pic.arrayBuffer()
 
